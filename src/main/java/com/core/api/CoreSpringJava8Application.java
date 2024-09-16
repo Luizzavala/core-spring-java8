dd949b08671d5e2b3e5140f03f6427606f5de6df
Luis Miguel Zavala <luisZavala@192.168.1.8>
2024-09-15 21:24:22 -0700

    init_()

---
+++ src/main/java/com/core/api/CoreSpringJava8Application.java
@@ -0,0 +1,13 @@
+package com.core.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreSpringJava8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreSpringJava8Application.class, args);
+	}
+
+}
